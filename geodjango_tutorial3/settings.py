e1c93a42db4b06a0b99c348f43da756bb5063498
Glenn271 <C17390923@mydit.ie>
2020-11-04 18:39:02 +0000

    changes

--- geodjango_tutorial3/settings.py
+++ geodjango_tutorial3/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import socket
+from secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
