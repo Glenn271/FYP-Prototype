08fb51bd47a64d013515a5e1ec46797ae831244c
Glenn271 <C17390923@mydit.ie>
2021-03-08 17:22:22 +0000

    icons.js file for custom icons

---
+++ world/static/world/icons.js
@@ -0,0 +1,19 @@
+    var leafIcon = L.Icon.extend({
+        options:{
+            iconSize: [30, 30]
+        }
+    });
+
+    var dinIcon = new leafIcon({iconUrl:'/static/images/utensils.svg'})
+
+    var edIcon = new leafIcon({iconUrl:'/static/images/school.svg'})
+
+    var entIcon = new leafIcon({iconUrl:'/static/images/theater-masks.svg'})
+
+    var finIcon = new leafIcon({iconUrl:'/static/images/euro.svg'})
+
+    var medIcon = new leafIcon({iconUrl:'/static/images/medkit.svg'})
+
+    var transIcon = new leafIcon({iconUrl:'/static/images/subway.svg'})
+
+    var houseIcon = new leafIcon({iconUrl:'/static/images/home.svg'})
