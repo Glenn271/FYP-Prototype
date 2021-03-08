4c3e7ea0362bf07d09973e8ec1907115c2cf026e
Glenn271 <C17390923@mydit.ie>
2021-03-08 19:50:06 +0000

    bootstrap and cleaning out old code

--- geodjango_tutorial3/urls.py
+++ geodjango_tutorial3/urls.py
@@ -20,16 +20,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
-from world import views as world_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('updatedb/', user_views.update_location, name='updatedb'),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('getprops/', world_views.search, name='search'),
     path('', include('world.urls')),
     path('', include('pwa.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -25,30 +25,3 @@ def profile(request):
     return render(request, 'users/profile.html')
 
 
-@login_required
-def update_location(request):
-    try:
-        print('in update location')
-        user = request.user
-        print(user)
-        user_profile = models.Profile.objects.get(user__username=user)
-        print(user_profile)
-        if not user_profile:
-            raise ValueError("Can't get User details")
-
-        print('user_profile')
-        print(user_profile)
-
-        point = request.POST["point"].split(",")
-        point = [float(part) for part in point]
-        point = Point(point, srid=4326)
-
-        print('point')
-        print(point)
-
-        user_profile.last_location = point
-        user_profile.save()
-
-        return JsonResponse({"message": f"Set location to {point.wkt})."}, status=200)
-    except Exception as e:
-        return JsonResponse({"message": str(e)}, status=400)
